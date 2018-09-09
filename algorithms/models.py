f197f1f63a85cff99090b7fee62262b3c4fc14bb
Misha <raraavis027@gmail.com>
2018-09-09 21:42:50 +1000

    first commit

---
+++ algorithms/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Algorithm(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    text = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
