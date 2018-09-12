3488d74e7e8c7dd6dc141de78eeb2ec85da1432d
Anton <toxaxab27@gmail.com>
2018-09-13 00:09:49 +1000

    Add footer

--- algorithms/models.py
+++ algorithms/models.py
@@ -6,4 +6,4 @@ class Algorithm(models.Model):
     description = models.CharField(max_length=300)
     text = models.TextField()
     pub_date = models.DateTimeField('date published')
-
+    author = models.CharField(max_length=200)
