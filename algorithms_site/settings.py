10b34a632c81076f29da353a6911e50837256e9c
Anton <toxaxab27@gmail.com>
2018-09-13 22:24:53 +1000

    delete accounts_app

--- algorithms_site/settings.py
+++ algorithms_site/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'accounts',
     'algorithms.apps.AlgorithmsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
